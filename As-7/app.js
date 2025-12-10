c0538f0158db984a0bea500fcfaaacaba865ebcd
Asadcoder786 <anonymousustad798@gmail.com>
2025-12-10 23:53:26 +0500

    JavaScript

---
+++ As-7/app.js
@@ -0,0 +1,256 @@
+// chapter 17 to 20
+// console.log("js is running")
+
+// Q1
+
+// let arr1 = [[],[],[]]
+// console.log(arr1)
+
+// Q2
+
+// let arr2 = [[1,2,3],[4,5,6],[7,8,9]]
+//     console.log(arr2)
+
+// Q3
+
+// for ( let i = 0 ; i <= 10 ; i++) {
+//     console.log(i)
+// }
+
+// Q4
+
+// let tableNumber = +prompt("Enter the table Number")
+// let tableLength = +prompt("Enter the table length")
+// for ( let i = 1 ; i <= tableLength; i++) {
+//     document.writeln( "<br>", tableNumber,"  ","x" , "  ", i ,"  ", "=", "  ",tableNumber*i ,"<hr>")
+// }
+ 
+
+// Q5
+
+// let fruit = ["Apple","Banana","Mango","Strawbarry"]
+// for (let i = 0; i < fruit.length ; i++) {
+//     console.log(fruit[i])
+// }
+
+// Q6 
+
+
+// for ( let i=1 ; i <=15 ; i++) {
+//      console.log(i)
+// }
+   
+
+// for ( let i=10 ; i >=1 ; i--) {
+//       console.log(i)
+// }
+  
+
+// for ( let i = 2; i <= 20 ; i += 2) {
+//     console.log(i)
+// }
+
+// for ( let i = 1; i <= 19 ; i += 2) {
+//     console.log(i)
+// }
+
+
+// for ( let i = 2; i <= 20 ; i += 2) {
+//     console.log(i + "k")
+// }
+
+// Q7
+
+
+
+// let sweets = ["cake","applepie","cookie","chips","patties"]
+// let usersweets = prompt("Welcome to the Sweets Bakery What do you want to eat")
+// for ( let i =0; i<sweets.length ; i++) {
+//     if (sweets[i] == usersweets) {
+//         console.log("This item is available in index",i,sweets)
+//     }
+//     else (
+//         console.log("This item is not available")
+//     )
+//     break;
+// }
+
+
+// Q8
+
+
+// let largestNumber = [24,53,78,91]
+// let userguessNumber= prompt("Guess the largest number 1 to 100")
+// for (let i = 1; i<largestNumber.length ; i++) {
+//     if (userguessNumber== 91 ) {
+//         alert("91 is the largest Number")
+//         break;
+//     }
+//     else{
+//         alert("Try again")
+//         break;
+//     }
+
+// }
+
+
+
+
+// Q9
+
+
+
+// let largestNumber = [24,53,78,91]
+// let userguessNumber= prompt("Guess the largest number 1 to 100")
+// for (let i = 1; i<largestNumber.length ; i++) {
+//     if (userguessNumber== 24 ) {
+//         alert("24 is the largest Number")
+//         break;
+//     }
+//     else{
+//         alert("Try again")
+//         break;
+//     }
+
+// }
+
+
+// Q10
+
+
+// for ( let i=5; i<=100; i +=5) {
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
