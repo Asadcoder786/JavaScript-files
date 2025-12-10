7cdbc5b7524636e69a3845d055fdb28f2afa5278
Asadcoder786 <anonymousustad798@gmail.com>
2025-12-10 23:52:51 +0500

    JavaScript

---
+++ As-5/script.js
@@ -0,0 +1,77 @@
+
+
+
+
+
+// alert("Welcome to ATM Machine \nHello user!");
+
+// let pinCode = 4527;
+
+// let userCode = prompt("Enter your pin")
+// if (pinCode == userCode) {
+//     console.log("Thk ha pin code paise lane ha")
+//     let kitneChiyeHa = prompt("Enter Yes or no")
+//     let kiaPaiseLaneHa = "yes";
+//     if (kiaPaiseLaneHa == kitneChiyeHa) {
+//         console.log("Kitne lane ha");
+//     } else {
+//         console.log("Phir kya karna aye hu idhar")
+//     }
+//     let AtmBalance = 50000;
+//     let userAmount = prompt("Enter your amount");
+//     let userWithdraw = prompt("Withdraw kitne karne ha");
+//     if (AtmBalance > userAmount) {
+//         console.log("ye amount nikl jaye gi apki", userAmount);
+//         if (userAmount > userWithdraw) {
+//             userAmount = userAmount - userWithdraw
+//             console.log("yE LO PASIE", userAmount);
+//         }
+//     } else {
+//         console.log("Apka Bank ma Itna Paisa Nahi Ha");
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
