68f82254b2ef32606edf4a4fd2bd6164b31ab32e
Asadcoder786 <anonymousustad798@gmail.com>
2025-12-10 23:54:40 +0500

    JavaScript

---
+++ As-9/app.js
@@ -0,0 +1,236 @@
+// chapter 26 to 30
+
+
+
+// Q1
+
+// let userInput = prompt("Enter a positive integer:")
+// let number = parseFloat(userInput)
+// let roundedNumber = Math.round(number)
+// let floorNumber = Math.floor(number)
+// let ceilNumber= Math.ceil(number)
+// document.writeln("Number:"+ number + "<br>")
+// document.writeln("Rounded Number:"+ roundedNumber + "<br>")
+// document.writeln("Floor Number:"+ floorNumber + "<br>")
+// document.writeln("Ceil Number:"+ ceilNumber + "<br>")
+
+
+
+// Q2
+
+
+
+
+
+
+// let userInput = prompt("Enter a negative integer:")
+// let number = parseFloat(userInput)
+// let roundedNumber = Math.round(number)
+// let floorNumber = Math.floor(number)
+// let ceilNumber= Math.ceil(number)
+// document.writeln("Number:"+ number + "<br>")
+// document.writeln("Rounded Number:"+ roundedNumber + "<br>")
+// document.writeln("Floor Number:"+ floorNumber + "<br>")
+// document.writeln("Ceil Number:"+ ceilNumber + "<br>")
+
+
+
+
+// Q3
+
+
+// let num1 = -4;
+// let num2 = 5;
+// console.log("The absolute value of ",num1,"is " ,Math.abs(num1))
+// console.log("The absolute value of ",num2,"is " ,Math.abs(num2))
+
+// Q4
+
+
+// let randomvalue = Math.random();
+// randomvalue = Math.floor(randomvalue * 6 )+1;
+// console.log(  "Random dice value:",randomvalue)
+// console.log("Random dice value:",Math.floor(Math.random() *6) +1)
+
+
+// Q5
+
+
+// let randomvalue = Math.random();
+// randomvalue = Math.floor(randomvalue *2) +1;
+// if (randomvalue === 2){
+//     console.log("Random coin value:Heads")
+// }
+// else {
+//     console.log("Random coin value:Tails")
+// }
+// console.log(randomvalue)
+
+
+// Q6
+
+
+// let randomvalue= Math.random();
+// randomvalue= Math.floor(randomvalue *100) +1;
+// console.log("Random number between 1 and 100:", randomvalue)
+
+// Q7
+
+
+
+let userInput = prompt("Enter your weight in kilograms:");
+let weight = parseFloat(userInput);
+console.log("The weight of user is ", weight, "kilograms");
+
+// Q8
+
+
+// let secretNumber = Math.floor(Math.random()*10)+1;
+// let userGuess = Number(prompt("Guess the secret number between 1 and 10"))
+// if (userGuess === secretNumber) {
+//     alert("Congratulations! you guessed the secret number.")
+
+// }else{
+//     alert("Try again")
+// }
+
+// console.log("Secret number is:", secretNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
