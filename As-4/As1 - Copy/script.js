d04d12677a293786c27818bc29d03b7f07323904
Asadcoder786 <anonymousustad798@gmail.com>
2025-12-10 23:50:41 +0500

    JavaScript

---
+++ As-4/As1 - Copy/script.js
@@ -0,0 +1,59 @@
+// -----------------------------------1st Assignment Start--------------------------------------------//
+
+// chapter1 //
+
+
+alert("Assalamualaikum \n I am a developer" )
+console.log("Assalamualaikum My name is Anonymous")
+
+
+// chapter2 //
+// Q1 to Q4 //
+
+
+let aboutUs = "I am a developer"
+let userName = "My name is Anonymous"
+let courseInformation = "I am Learning full stack developer"
+
+alert(aboutUs)
+alert(userName)
+alert(courseInformation)
+
+// Q5 //
+var Pizza = "Pizza\n" + "Pizz\n" + "Piz\n" + "Pi\n" + "P"
+alert(Pizza)
+// Q6 //
+const email = "Anonymous@example.com"
+alert(email)
+// Q7 //
+let books = "A smarter way to learn Javascript"
+alert(books)
+//  Q8 //
+document.writeln("<h2>Yah ! I can write HTML content through JavaScript</h2>")
+// Q9 //
+var line = "<==----------Geo--------===>"
+alert(line)
+
+// chapter3 //
+// Q1 //
+const age = "I am 15 year old"
+alert(age)
+// Q2 //
+let init 
+init = "I have visited this website 15 times"
+alert(init)
+// Q3 //
+var birth = "My birth year is 1990"
+const day  = "My birth day is jumma"
+document.writeln("<br>" + birth + "</br>")
+document.writeln(day)
+// Q4 //
+let username = "Anonymous"
+let produstname = "Nike Shirt"
+let quantity = "!5 shirts"
+document.writeln(   "<br>"  + username + "</br>")
+document.writeln( "<br>"  + produstname + "</br>")
+document.writeln(  "<br>"  + quantity + "</br>")
+
+// -----------------------------------1st Assignment End--------------------------------------------//
+
