3a78c5391d5a526e0e0c2fa926db0eb87a680f2b
Asadcoder786 <anonymousustad798@gmail.com>
2025-12-13 00:15:30 +0500

    Light and Dark Mode Logic

---
+++ Dark&lightMode/app.js
@@ -0,0 +1,20 @@
+
+
+let dark= false;
+function changeMode() {
+   if(dark===false) {
+    document.body.style.background="black"
+    document.body.style.color="white"
+    dark=true;
+   }else {
+  document.body.style.background="white"
+  document.body.style.color="black"
+  dark=false;
+}
+}
+
+
+
+
+
+
